3529c9dfecbbbbf3d8e42d30c25b83f8615a7624
caspero-62 <nwaforuchechukwu2007@gmail.com>
2021-08-15 10:33:23 +0100

    final chnages

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 const navbarSticky = document.querySelector('#nav2');
 const main = document.querySelector('main');
-
+const loaders = document.querySelectorAll('.loader');
 const stickyPosition = navbarSticky.offsetTop;
 
 const navbarStickyMobile = document.querySelector('#nav2-mobile');
@@ -97,7 +97,9 @@ const fillProfileSection = (data) => {
     <div class="small-none profile-picture">
       <img src="${avatarUrl}" alt="profile-picture">
       <div class="status-emoji">
-          ${status.emojiHTML}
+          <span class="emoji">
+            ${status.emojiHTML}
+          </span>
           <span class="edit-status"><a href="#">&nbsp; Edit status</a></span>
       </div>
     </div>
@@ -140,12 +142,12 @@ const fillProfileSection = (data) => {
               <span class="stat-number">${followers.totalCount}</span>
               <span>followers</span>
           </a>
-          <span class="dot">.</span>
+          <span class="dot">&middot;</span>
           <a href="#">
               <span class="stat-number">${following.totalCount}</span>
               <span>following</span>
           </a>
-          <span class="dot">.</span>
+          <span class="dot">&middot;</span>
           <a href="#">
               <svg class='star' height="16" viewBox="0 0 16 16" version="1.1" width="16" aria-hidden="true"><path fill-rule="evenodd" d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"></path></svg>
               <span class="stat-number">${starredRepositories.totalCount}</span>
@@ -396,39 +398,40 @@ const queryData = `{
 
 const API_URL = 'https://api.github.com/graphql';
 
-const token = process.env.token;
+const token = '';
 
 const queryOptions = {
     method: "POST",
     headers: {
-        "Authorization": `bearer ${token}`,
-        "Content-Type": "application/json"
+      "Authorization": `bearer ${token}`,
+      "Content-Type": "application/json"
     },
     body: JSON.stringify({
-        query: queryData
+      query: queryData
     }),
 }
 
 const getRepositoryData = async () => {
     try {
-        
-        const response = await fetch(API_URL, queryOptions);
-        const fetchedData = await response.json();
-        const reqData = fetchedData.data.user;
+      loaders.forEach(loader => loader.className = 'loader')
+      const response = await fetch(API_URL, queryOptions);
+      const fetchedData = await response.json();
+      const reqData = fetchedData.data.user;
+      const {
+        repositories,
+      } = reqData;
 
-        const {
-          repositories,
-        } = reqData;
+      fillProfileSection(reqData);
+      fillRepositoryData(repositories.nodes);
+      
+      repoNumber.forEach(num => {
+        num.innerHTML = repositories.totalCount;
+      });
 
-        fillProfileSection(reqData);
-        fillRepositoryData(repositories.nodes);
-        repoNumber.forEach(num => {
-          num.innerHTML = repositories.totalCount;
-        })
-
+      loaders.forEach(loader => loader.className = 'loader none');
     } catch (err) {
-
-       console.log(err);
+      loaders.forEach(loader => loader.className = 'loader none');
+      console.log(err);
 
     }
 }
@@ -458,5 +461,4 @@ window.addEventListener('scroll', (e) => {
 hamburger.addEventListener('click', showMenu);
 
 // =============================== Event Listeners End ================================//
-
 
